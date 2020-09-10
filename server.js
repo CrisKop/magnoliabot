6f6ecc6b0d90628e8825523fff90e5c0c3609342
Glitch (hello-express) <none>
2020-09-10 13:35:15 +0000

    🍉🚖 Checkpoint ./mega_databases/color.json:8710627/37 ./mega_databases/cooldown2.json:8710627/114 ./mega_databases/cooldown3.json:8710627/73 ./mega_databases/Descripciones.json:8710627/35 ./mega_databases/Developers.json:8710627/76 ./mega_databases/Dinero.json:8710627/119 ./mega_databases/niveles.json:8710627/371 ./mega_databases/Vips.json:8710627/70 ./package.json:8710627/5463 ./watch.json:8710627/217 ./server.js:8710627/3368 ./niveles.js:8710627/1861

--- server.js
+++ server.js
@@ -1,34 +1,68 @@
-// server.js
-// where your node app starts
-
-// we've started you off with Express (https://expressjs.com/)
-// but feel free to use whatever libraries or frameworks you'd like through `package.json`.
+const http = require("http");
 const express = require("express");
 const app = express();
-
-// our default array of dreams
-const dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
-
-// make all the files in 'public' available
-// https://expressjs.com/en/starter/static-files.html
-app.use(express.static("public"));
-
-// https://expressjs.com/en/starter/basic-routing.html
 app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/views/index.html");
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://NameProject.glitch.me/`);
+}, 280000);
+
+
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const { nivelesFunc } = require("./niveles.js");
+
+client.on("ready", () => {
+  console.log("Estoy listo!");
+  setInterval(function() {
+    let statuses = [`f/ayuda 🎸 ${client.guilds.cache.size}`, `f/comandos 👤 ${client.users.cache.size}`];
+    let status = Math.floor(Math.random() * statuses.length);
+    let dstatus = statuses[status];
+    client.user.setPresence({
+      activity: {
+        name: `${dstatus}`,
+        type: "LISTENING"
+      },
+      status: "online"
+    });
+  }, 5000);
 });
 
-// send the default array of dreams to the webpage
-app.get("/dreams", (request, response) => {
-  // express helps us take JS objects and send them as JSON
-  response.json(dreams);
+client.on("message", async message => {
+  let prefix = "f/";
+  if (message.author.bot) return;
+  if (message.content.indexOf(prefix) !== 0) {
+    nivelesFunc(message);
+    return;
+  }
+  
+  const args = message.content
+    .slice(prefix.length)
+    .trim()
+    .split(/ +/g);
+  const command = args.shift().toLowerCase();
+  
+  try {
+    let comandos = require(`./cmd/${command}.js`);
+    comandos.run(client, message, args);
+  } catch (e) {
+    console.log(e.stacks);
+    message.channel.send(
+      new Discord.MessageEmbed()
+        .setColor("RED")
+        .setThumbnail(
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a2/Nuvola_apps_error.svg/1200px-Nuvola_apps_error.svg.png"
+        )
+        .setDescription("`Comando que uso no existe en mi base de datos`")
+        .addField(
+          "`Trata usando el siguiente comando`",
+          "☑️ **" + prefix + "comandos**"
+        )
+    );
+  } finally {
+  }
 });
 
-// listen for requests :)
-const listener = app.listen(process.env.PORT, () => {
-  console.log("Your app is listening on port " + listener.address().port);
-});
+client.login(process.env.TOKEN)
