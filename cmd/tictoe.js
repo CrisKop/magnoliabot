d2cf77df1a3ce35f28eefef9ac232229013b2ffb
Glitch (hello-express) <none>
2020-09-11 13:43:15 +0000

    🎫👞 Checkpoint ./cmd/tictoe.js:8710627/4868 ./package.json:8710627/1626 ./watch.json:8710627/6

--- cmd/tictoe.js
+++ cmd/tictoe.js
@@ -1,54 +1,24 @@
-exports.run = (client, message, args) => {
 const tresenraya = require('tresenraya');
- let Discord = require("discord.js")
-const user = message.mentions.users.first();
-const mencion = new Discord.RichEmbed()
-  .setAuthor(`🎲 Juego TicTacToe 🎲`)
-  .setColor("RED")
-  .setDescription("❌ Mencion a un usuario para jugar")
-  if(!user) return message.channel.send(mencion)
-
-  var embot = new Discord.RichEmbed();
-  embot.setDescription("❌ No puedes jugar con un bot");
-  embot.setColor("RED")
-  if(user.bot) return message.channel.send(embot);
-
-  if (user.id == message.author.id)
-  return message.channel.send(
-    new Discord.MessageEmbed()
-      .setDescription("❌ No puedes jugar contigo mismo")
-      .setColor("RED")
-  );
+ 
+exports.run = async (client, message, args) => {
+const usuario = message.mentions.users.first();
+if(!usuario) return message.channel.send('Menciona a alguien');
   
-const partida = new tresenraya({ jugadores: [message.author.id, user.id] });
+const partida = new tresenraya.partida({ jugadores: [message.author.id, usuario.id] });
   
 partida.on('ganador', (jugador, tablero, paso) => { // cuando encuentra a algún ganador se emite el evento 'ganador'
     
-  message.channel.send(
-    new Discord.MessageEmbed()
-    .setColor("GREEN")
-    .setAuthor(`🎲 Juego TicTacToe (GANASTE) 🎲`)
-    .setDescription('¡Ha ganado ' + client.users.cache.get(jugador).username + ' en esta partida! Después de `' + paso + ' pasos.`\n\n' + tablero.string + '\n\nLo siento, ' + client.users.get(partida.perdedor).username + '... :frowning:')
-  )
+  message.channel.send('¡Ha ganado ' + client.users.cache.get(jugador).username + ' en esta partida! Después de `' + paso + ' pasos.`\n\n' + tablero.string + '\n\nLo siento, ' + client.users.cache.get(partida.perdedor).username + '... 😦');
     
 });
   
 partida.on('empate', (jugadores, tablero, paso) => { // si se produce un empate se emite el evento 'empate'
     
-  message.channel.send(
-    new Discord.MessageEmbed()
-    .setAuthor(`🎲 Juego TicTacToe (EMPATE) 🎲`)
-    .setColor("ORANGE")
-    .setDescription('¡Ha sido un empate entre: ' + jugadores.map(x => client.users.cache.get(x).username).join(' y ') + '!')
-  )
+  message.channel.send('¡Ha habido un empate entre ' + jugadores.map(x => client.users.get(x).username).join(' y ') + '!');
+    
 });
   
-message.channel.send(
-  new Discord.MessageEmbed()
-  .setColor("BLUE")
-    .setAuthor(`🎲 Juego TicTacToe (COMIENZO) 🎲`)
-    .setDescription('Empieza ' + client.users.cache.get(partida.turno.jugador).username + ', elige un número del 1 al 9 [`' + partida.turno.ficha + '`]\n\n' + partida.tablero.string)
-  )
+message.channel.send('Empieza ' + client.users.cache.get(partida.turno.jugador).username + ', elige un número del 1 al 9 [`' + partida.turno.ficha + '`]\n\n' + partida.tablero.string);
  
 const colector = message.channel.createMessageCollector(msg => msg.author.id === partida.turno.jugador && !isNaN(msg.content) && (Number(msg.content) >= 1 && Number(msg.content) <= 9) && partida.disponible(msg.content) && !partida.finalizado);
  
@@ -63,12 +33,7 @@ colector.on('collect', (msg) => {
     
   } // si la partida ya ha finalizado (ya sea por que alguien ha ganado o ha habido un empate), para el colector y retorna nada
       
-  message.channel.send(
-    new Discord.MessageEmbed()
-    .setColor("PURPLE")
-    .setAuthor(`🎲 Juego TicTacToe (TURNOS) 🎲`)
-    .setDescription('Turno de ' + client.users.cache.get(partida.turno.jugador).username + ' [`' + partida.turno.ficha + '`]\n\n' + partida.tablero.string)
-  );
+  message.channel.send('Turno de ' + client.users.cache.get(partida.turno.jugador).username + ' [`' + partida.turno.ficha + '`]\n\n' + partida.tablero.string);
       
 });
 }
