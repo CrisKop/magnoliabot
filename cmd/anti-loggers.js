0fc69bd122f198c60d3e856da5c5847ab281d51c
Glitch (hello-express) <none>
2020-09-17 19:43:14 +0000

    🏅👛 Checkpoint ./cmd/anti-loggers.js:8710627/4373 ./watch.json:8710627/22

--- cmd/anti-loggers.js
+++ cmd/anti-loggers.js
@@ -1,39 +1,54 @@
 const Discord = require("discord.js");
 
-exports.run = (client, message, args) => {
-    let xd = args.slice(0).join(" ");
+exports.run = async (client, message, args) => {
+  let xd = args.slice(0).join(" ");
 
-    let db = require("megadb")
-    let al = new (require("megadb")).crearDB("AntiLoggers");
-    const log = new db.crearDB("Logs")
-    let perms = message.member.hasPermission("ADMINISTRATOR");
-
-    if (!perms)
-      return message.channel.send(
-        "❌ No tienes permisos de `Administrador`."
-      );
-
-    if (!xd)
-      return message.channel.send(
-        "☑️ Activa usando `anti-loggers On` \n❌ Desactiva usando `anti-loggers Off`"
-      );
-
-    if (args[0] === "Off") {
-      al.eliminar(`${message.guild.id}`);
-      return message.channel.send(
-        "☑️ AntiLoggers **Desactivada** Correctamente"
-      );
-    } else if (args[0] === "On") {
-      if(al.tiene(message.guild.id)) return message.channel.send("☑️ Los Anti-Loggers Ya estan activados")
-      al.establecer(`${message.guild.id}.at`, "activado");
-      canalrendered.send(
-      new Discord.MessageEmbed()
-      .setAuthor(`${client.user.username} Filtros`, client.user.displayAvatarURL())
-      .addField("Accionado Por:", message.author.username)
-      .addField("Filtro:", "`Anti-Loggers`")
-      .setThumbnail(client.user.di))
-      return message.channel.send(
-        "☑️ AntiLoggers **Activada** Correctamente"
-      );
-    }
+  let db = require("megadb");
+  let al = new (require("megadb")).crearDB("AntiLoggers");
+  const log = new db.crearDB("Logs");
+  let l = await log.obtener(message.guild.id)
+   let we;
+  if (log.tiene(`${message.guild.id}`)) {
+    we = `Activado: <#${l}>`;
   }
+
+  if (!log.tiene(`${message.guild.id}`)) {
+    we = "❌ **Canal no definido**";
+  }
+  
+  let perms = message.member.hasPermission("ADMINISTRATOR");
+
+  if (!perms)
+    return message.channel.send("❌ No tienes permisos de `Administrador`.");
+
+  //if(!log.tiene(`${message.guild.id}`)) return message.channel.send("❌ No has establecido el canal de logs \nUsa: `setlogs #canal`")
+  if (!xd)
+    return message.channel.send(
+      "☑️ Activa usando `anti-loggers On` \n❌ Desactiva usando `anti-loggers Off`"
+    );
+
+  if (args[0] === "Off") {
+    al.eliminar(`${message.guild.id}`);
+    return message.channel.send("☑️ AntiLoggers **Desactivada** Correctamente");
+  } else if (args[0] === "On") {
+    if (al.tiene(message.guild.id))
+      return message.channel.send("☑️ Los Anti-Loggers Ya estan activados");
+    al.establecer(`${message.guild.id}.at`, "activado");
+    if (!log.tiene(`${message.guild.id}`)) return;
+    let logs = await log.obtener(`${message.guild.id}`);
+    const canalrendered = client.channels.cache.get(logs);
+    canalrendered.send(
+      new Discord.MessageEmbed()
+        .setAuthor(
+          `🌐 ${client.user.username} Filtros 🌐`,
+          client.user.displayAvatarURL()
+        )
+        .setDescription("🍬 `|` **_Filtro Anti-Loggers Activado_** \n`Canal:` "+ `${we}`)
+        .addField("👮 `|` **__Activado Por:__**", `${message.author}`)
+        .addField("☄️ `|` **__Filtro:__**", "`Anti-Loggers`")
+        .setThumbnail(client.user.displayAvatarURL())
+        .setColor("RED")
+    );
+    return message.channel.send("☑️ AntiLoggers **Activada** Correctamente");
+  }
+};
