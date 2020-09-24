5665f4c378562b9db4fb6753ed7a72cc2a30e71b
Glitch (falexy-bot) <none>
2020-09-24 00:12:52 +0000

    🎁💅 Checkpoint ./server.js:8710627/1820

--- server.js
+++ server.js
@@ -227,38 +227,37 @@ client.on(`guildCreate`, (guild, message) => {
 });
 
 client.on("message", async message => {
-  let { crearDB } = require("megadb"); // Llamamos al constructor crearDB
-  let palta = new crearDB("palta"); // Creamos el archivo json en el que almacenaremos los datos de los usuarios AFK
+  let { crearDB } = require("megadb");
+  let palta = new crearDB("palta");
   let Discord = require("discord.js");
   if (palta.tiene(message.guild.id)) {
-    //Verificamos si el archivo json tiene datos guardados con la ID del servidor donde se mando el mensaje
-    let lista = await palta.get(message.guild.id); //Obtenemos el array guardado con la lista de los usuarios AFK en el servidor
+    
+    let lista = await palta.get(message.guild.id);
     if (lista.includes(message.author.id)) {
-      //Verificamos si el array incluye la ID del autor del mensaje
-      palta.extract(message.guild.id, message.author.id); //Eliminamos el elemento con la ID del autor del mensaje del array
-      message.channel.send(`${message.author} has vuelto de tu AFK!`
-      ); //Enviamos un mensaje avisandole al autor del mensaje que ya no esta AFK
-    }
-  } //Cerramos
-  //Lo de arriba va al inicio del evento message y arriba de cualquier condiciÃ³n que retorne si el contenido del mensaje no empieza por X caracter, por ejemplo if(!message.content.startsWith(prefix)) return;
 
+      palta.extract(message.guild.id, message.author.id); 
+      message.channel.send(`${message.author} has vuelto de tu AFK!`
+      ); 
+    }
+  }
+  
   if (message.mentions.users.first() && palta.tiene(message.guild.id)) {
-    //Verificamos si el mensaje tiene una mención y el archivo json datos
-    let lista = await palta.get(message.guild.id); //Obtenemos el array con la lista de usuarios AFK nuevamente
-    let text = []; //Declaramos text como un array vació
+    
+    let lista = await palta.get(message.guild.id); 
+    let text = []; 
     message.mentions.users.forEach(x => {
-      //Utilizamos forEach para repetir una acción con todas las menciones
+
       if (lista.includes(x.id)) {
-        //Verificamos si el array de los usuarios AFK incluye la ID de los usuarios mencionados
-        text.push("<@" + x.id + ">"); //Añadimos la ID de los usuarios AFK al array text para mencionarles luego
+        
+        text.push("<@" + x.id + ">"); 
       }
     });
     if (text.length >= 1) {
-      //Verificamos si la longitud del array es mayor o igual a 1 en caso de que ninguno de los usuarios mencionados estuviera AFK
-      let estan = text.length > 1 ? "están" : "esta"; //Esto es por simple estetica, Utilizamos un operador ternario para verificar si text tiene más de 1 elemento, en caso de que si nuestra variable será igual a "están", en caso contrario "esta"
-      message.channel.send("¡" + text.join(" ") + " " + estan + " afk!"); //Enviamos el mensaje, utilizamos text.join(' ') para unir el array con un espacio
+      
+      let estan = text.length > 1 ? "están" : "esta";
+      message.channel.send("¡" + text.join(" ") + " " + estan + " afk!");
     }
-  } //Cerramos
+  }
 
   if (message.content.startsWith("f/afk")) {
   const db = require("megadb");
@@ -278,9 +277,9 @@ client.on("message", async message => {
   const command = args.shift().toLowerCase();
     
     let razon = args.slice(0).join(" ") || "Razon Indefinida"
-    //Verificamos si el contenido del mensaje empieza con "!afk"
-    if (!palta.tiene(message.guild.id)) palta.establecer(message.guild.id, []); //Verificamos si el archivo json tiene datos guardados con la ID del servidor, En caso de que no establecemos un array
-    palta.push(message.guild.id, message.author.id); //Añadimos la ID del autor del mensaje al array
+
+    if (!palta.tiene(message.guild.id)) palta.establecer(message.guild.id, []); 
+    palta.push(message.guild.id, message.author.id);
     message.channel.send(
       new Discord.MessageEmbed()
         .setAuthor(message.author.tag)
