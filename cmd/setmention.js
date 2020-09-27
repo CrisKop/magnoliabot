9c1af9cfc81ea49b1e185c5e57e973de264673d9
Glitch (falexy-bot) <none>
2020-09-27 20:21:39 +0000

    ⭐️🙉 Checkpoint ./cmd/setmention.js:8710627/1508 ./watch.json:8710627/2

--- cmd/setmention.js
+++ cmd/setmention.js
@@ -1,10 +1,26 @@
 exports.run = async (client, message, args) => {
-  let Discord = require("discord.js")
-  const db = require("megadb")
-  const mencion = new db.crearDB("MencionSuggest")
-  let role = message.mentions.roles.first()
-  if(!role) return message.channel.send("❌ `|` **Debes mencionar un role para configurarlo**")
+  let Discord = require("discord.js");
+  const db = require("megadb");
+  const mencion = new db.crearDB("MencionSuggest");
+
+  if(!args[0]) return message.channel.send(
+  new Discord.MessageEmbed()
+  .setAuthor(`📛 | Ocurrio Un Error... | 📛`, client.user.displayAvatarURL())
+  .setDescription("❌ `|` **__Para definir el role usa:__** `setmention sugerencia @role`")
+  .setColor("RANDOM")
   
-  mencion.establecer(message.guild.id, role.id)
-  message.channel.send("☑️ `|` **Role establecido correctamente para mencionar en las sugerencias** "+`<@&${role.id}>`)
-}
+  )
+  if (args[0] === "sugerencia") {
+    let role = message.mentions.roles.first();
+    if (!role)
+      return message.channel.send(
+        "❌ `|` **Debes mencionar un role para configurarlo**"
+      );
+
+    mencion.establecer(message.guild.id, role.id);
+    message.channel.send(
+      "☑️ `|` **Role establecido correctamente para mencionar en las sugerencias** " +
+        `<@&${role.id}>`
+    );
+  }
+};
