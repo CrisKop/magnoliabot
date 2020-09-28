df0e8af8e119e25786ffd02bafec87bfd5cacbd6
Glitch (falexy-bot) <none>
2020-09-28 12:24:11 +0000

    🍗🏏 Checkpoint ./package.json:8710627/11302 ./watch.json:8710627/4 ./server.js:8710627/426

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const http = require("http");
 const express = require("express");
 const app = express();
+
+app.use(express.static("public"));
+
+app.get("/", function(request, response) {
+  response.sendFile(__dirname + "/views/index.html");
+});
+
 app.get("/", (request, response) => {
   response.sendStatus(200);
 });
+
 app.listen(process.env.PORT);
+
 setInterval(() => {
-  http.get(`http://falexyEco.glitch.me/`);
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
 }, 280000);
 
 const Discord = require("discord.js");
