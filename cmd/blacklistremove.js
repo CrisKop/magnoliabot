31f28f91001f138b5f3080ce4eff65001e4a5833
Glitch (falexy-bot) <none>
2020-09-28 19:06:28 +0000

    🍯💕 Checkpoint ./cmd/blacklistremove.js:8710627/3468 ./watch.json:8710627/2

--- cmd/blacklistremove.js
+++ cmd/blacklistremove.js
@@ -1,7 +1,7 @@
 exports.run = async (client, message, args) => {
-    const Discord = require("discord.js");
-      const db = require("megadb");
-      const developers_db = new db.crearDB("Developers");
+  const Discord = require("discord.js");
+  const db = require("megadb");
+  const developers_db = new db.crearDB("Developers");
   let devs = await developers_db.obtener("Developers");
   let user = message.author;
   if (devs.includes(user.id) == false)
@@ -16,24 +16,30 @@ exports.run = async (client, message, args) => {
           "🗳️ `|` **__No eres STAFF del BOT, por lo tanto no puedes usar el comando__**"
         )
     );
-      let blacklist = new db.crearDB("BlackList");
+  let blacklist = new db.crearDB("BlackList");
   let blackrazon = new db.crearDB("Razones");
-      if (!blacklist.tiene("blacklist")) {
-        blacklist.establecer("blacklist", []);
-      } //Guardamos los argumentos 0 (id) en el array creado anteriormente El codigo este va en el comando el de arriba en otro para crearlo
-  
-      if (!args[0]) return message.channel.send("❌ Debe colocar una ID");
-      const idsxd = await blacklist.obtener("blacklist");
-      if (idsxd.includes(args[0]) == false)
-        return message.channel.send("❌ El usuario no esta en la forceban");
+  if (!blacklist.tiene("blacklist")) {
+    blacklist.establecer("blacklist", []);
+  } //Guardamos los argumentos 0 (id) en el array creado anteriormente El codigo este va en el comando el de arriba en otro para crearlo
 
-    if(user.bot) return message.channel.send("No puedes agregar a un bot")
-      if (isNaN(args[0]))
-        return message.channel.send("❌ Debe colocar una ID real");
-  let xd = args.slice(1).join(" ") 
-      blacklist.extract("blacklist", args[0]);
-      blackrazon.extract(args[0], xd)
-  
-     message.channel.send(`**__Se ha eliminado un usuario de la blacklist__** <@${args[0]}> \`\`[${args[0]}]\`\``)
-    client.users.cache.get(args[0]).send("**__Has sido eliminado de la blacklist__**")
-    }
+  if (!args[0]) return message.channel.send("❌ Debe colocar una ID");
+  const idsxd = await blacklist.obtener("blacklist");
+  if (idsxd.includes(args[0]) == false)
+    return message.channel.send("❌ El usuario no esta en la forceban");
+
+  if (user.bot) return message.channel.send("No puedes agregar a un bot");
+  if (isNaN(args[0]))
+    return message.channel.send("❌ Debe colocar una ID real");
+  let xd = args.slice(1).join(" ");
+  blacklist.extract("blacklist", args[0]);
+  blackrazon.extract("Razones", xd);
+
+  message.channel.send(
+    `**__Se ha eliminado un usuario de la blacklist__** <@${args[0]}> \`\`[${
+      args[0]
+    }]\`\``
+  );
+  client.users.cache
+    .get(args[0])
+    .send("**__Has sido eliminado de la blacklist__**");
+};
