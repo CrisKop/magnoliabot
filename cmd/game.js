dffcf489dc7c5a885d262cdbc1c7e1d477f4702b
Glitch (falexy-bot) <none>
2020-09-29 16:25:36 +0000

    🦁🐡 Checkpoint ./cmd/game.js:8710627/6962 ./watch.json:8710627/16

--- cmd/game.js
+++ cmd/game.js
@@ -6,6 +6,15 @@ exports.run = async (client, message, args) => {
   const dbs = require("quick.db");
   const ms = require("parse-ms");
 
+  let prefix_db = new db.crearDB("prefixes");
+
+  let prefix;
+  if (prefix_db.tiene(`${message.guild.id}`)) {
+    prefix = await prefix_db.obtener(`${message.guild.id}`);
+  } else {
+    prefix = "f/";
+  }
+
   let user = message.author;
   let author = await dbs.fetch(`game_${message.guild.id}_${user.id}`);
   let timeout = 180000;
@@ -19,26 +28,64 @@ exports.run = async (client, message, args) => {
       );
     message.channel.send(timeEmbed);
   } else {
-    let palabras = ["ElSkay", "Dinero", "Banco", "La Casa De Papel", "Sex Education.", "Elite", "Seguridad", "Falexy", "Wolf", "Fargan", "Wolf Security", "Barcelona", "Parangaricutirimicuaro", "GaTos", "Perros", "MineCraft", "Youtube", "Netflix", "Monkey Security", "Monkey", "Pepe la Rana", "El Pepino"];
+    let palabras = [
+      "ElSkay",
+      "Dinero",
+      "Banco",
+      "La Casa De Papel",
+      "Sex Education.",
+      "Elite",
+      "Seguridad",
+      "Falexy",
+      "Wolf",
+      "Fargan",
+      "Wolf Security",
+      "Barcelona",
+      "Parangaricutirimicuaro",
+      "GaTos",
+      "Perros",
+      "MineCraft",
+      "Youtube",
+      "Netflix",
+      "Monkey Security",
+      "Monkey",
+      "Pepe la Rana",
+      "El Pepino"
+    ];
     let palabra = palabras[Math.floor(Math.random() * palabras.length)];
     let filtro = m => m.content === palabra;
     message.channel.send(
       new Discord.MessageEmbed()
-        .setThumbnail("https://cdn.discordapp.com/attachments/760318470112935936/760519980906446918/3520277.png")
+        .setColor("ORANGE")
+        .setThumbnail(
+          "https://cdn.discordapp.com/attachments/760318470112935936/760519980906446918/3520277.png"
+        )
         .setAuthor(`🎲 | Juego Rapido | 🎲`, client.user.displayAvatarURL())
         .setDescription(
           "🎮 `|` **__Tienes que escribir la palabra que se muestra rapidamente para ganar__**"
         )
         .addField("📜 `|` **__Palabra:__**", "`" + palabra + "`")
     );
+    
+     Math.floor(Math.random() * (1001 - 100)) + 100;
+    let rdm = Math.floor(Math.random() * (1001 - 100)) + 100;
+
+    
     message.channel
       .awaitMessages(filtro, { max: 1, time: 60000, errors: ["time"] })
       .then(msg => {
         message.channel.send(
-        new Discord.MessageEmbed()
-        .setAuthor(`Ganaste ${message.aut`));
-        dinero.sumar(`${message.guild.id}.${message.author.id}`, 100);
-      dbs.set(`game_${message.guild.id}_${user.id}`, Date.now());
+          new Discord.MessageEmbed()
+          .setThumbnail("https://cdn.discordapp.com/attachments/758153151508643890/760536717391953990/3435908.png")
+          .setColor("GREEN")
+            .setAuthor(
+              `🎉 | Ganaste ${msg.first().author.username} | 🎉`)
+            .setDescription(
+              "💝 `|` **__Felicidades, has ganado el juego, y eres un master en la escritura__** \nGanaste `"+rdm+"` De dinero, Usa: `"+prefix+"perfil`"
+            )
+        );
+        dinero.sumar(`${message.guild.id}.${message.author.id}`, rdm);
+        dbs.set(`game_${message.guild.id}_${user.id}`, Date.now());
       })
       .catch(() => {
         message.channel.send(`Ninguno lo logro...`);
